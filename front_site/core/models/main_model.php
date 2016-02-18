00a791b432817887ad542b4cebcea32c54b5edd1
MargioOne <babazinavkorzini1941@gmail.com>
2016-02-18 23:05:36 +0200

    первые шаги 4
    первые шаги 4

---
+++ front_site/core/models/main_model.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Pavel
+ * Date: 18.02.2016
+ * Time: 13:09
+ */
+class main_model
+{
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel
+ * Date: 17.02.2016
+ * Time: 16:37
+ */
+
+header ("Location: front_site/index.php");
