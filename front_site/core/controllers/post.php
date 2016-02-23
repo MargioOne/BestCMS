5600b87f40344b4e2acb658da0018e244291a2eb
MargioOne <babazinavkorzini1941@gmail.com>
2016-02-23 10:16:03 +0200

    день3

--- front_site/core/controllers/post.php
+++ front_site/core/controllers/post.php
@@ -9,25 +9,33 @@
  */
 class post
 {
-    public $title;
+    public $title, $article;
+
 
     function __construct() //конструктор
     {
-        $this->get_article();
+        $this->get_article_and_pagination();
     }
 
-    function get_article() //получение данных статьи
+    function get_article_and_pagination() //получение данных статьи
     {
+        $_GET['articles'] = 1;
+        $num = 2;
+        $articles = $_GET['articles'];
+        $articles = intval($articles);
         include(MODELPATCH . "model_post.php");
         $model = new view_post();
-        $model->get_article();
-        // В $article_row хранятся данные с базы таблицы article.
+        $model->get_count_posts();
+        $total_pages = intval(($model->count_posts - 1) / $num) + 1;
+        if (empty($articles) || $articles < 0) $articles = 1;
+        if ($articles > $total_pages) $articles = $total_pages;
+        $start = $articles * $num - $num;
+        $model->get_article($start, $num);
+        echo $model->count_posts;
     }
 
     function get_title()
     {
 
     }
-
-
 }
--- front_site/core/models/model_post.php
+++ front_site/core/models/model_post.php
@@ -9,13 +9,26 @@
 class view_post
 {
     public $article_row;
+    public $count_posts;
+    public $result;
 
-    function get_article()
+    function get_article( $start, $num)
     {
         $db = new mysqli(HOSTBD, USERBD, PASSBD, NAMEBD);
-        $result = $db->query('SELECT * FROM article') or die("ERROR: " . mysql_error());;
-        $this->article_row = mysqli_fetch_assoc($result);
-        //return $this->article_row;
+        $this->result = $db->query('SELECT * FROM article LIMIT 1, 2');
+        //while(!empty($result)){$this->article_row = mysqli_fetch_assoc($result);}
+
+        return $this->result;
+
+
     }
 
+    function get_count_posts()
+    {
+        $db = new mysqli(HOSTBD, USERBD, PASSBD, NAMEBD);
+        $result = $db->query('SELECT COUNT(*) FROM article');
+        $this->count_posts = mysqli_fetch_assoc($result);
+        $this->count_posts = intval($this->count_posts);
+        return $this->count_posts;
+    }
 }
